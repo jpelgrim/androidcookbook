fc656c00842b69db0475a57e0043e286f3f2e24a
Johan Pelgrim <johan.pelgrim@gmail.com>
2011-04-18 22:59:11 +0200

    Modified api-key and set build in zoom control of the MapView to true

--- MapTest/src/nl/codestone/cookbook/maptest/MapTest.java
+++ MapTest/src/nl/codestone/cookbook/maptest/MapTest.java
@@ -3,18 +3,22 @@ package nl.codestone.cookbook.maptest;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class MapTest extends MapActivity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        MapView mapview = (MapView) findViewById(R.id.mapview);
+        mapview.setBuiltInZoomControls(true); 
+    
     }
 
     @Override
     protected boolean isRouteDisplayed() {
-        // TODO Auto-generated method stub
         return false;
     }
 }
