5700a9fe7423e56376bbd36ac93ee2d16c66556e
Johan Pelgrim <johan.pelgrim@gmail.com>
2011-08-04 22:27:04 +0200

    Aligned comment line numbers with on-line recipe

--- IncomingCallInterceptor/src/nl/codestone/cookbook/incomingcallinterceptor/IncomingCallInterceptor.java
+++ IncomingCallInterceptor/src/nl/codestone/cookbook/incomingcallinterceptor/IncomingCallInterceptor.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
-public class IncomingCallInterceptor extends BroadcastReceiver {
+public class IncomingCallInterceptor extends BroadcastReceiver {                                    // 1
 
     @Override
-    public void onReceive(Context context, Intent intent) {                                         // 1
-        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);                         // 2
+    public void onReceive(Context context, Intent intent) {                                         // 2
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);                         // 3
         String msg = "Phone state changed to " + state;
         
-        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {                                   // 3
-            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);  // 4
+        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {                                   // 4
+            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);  // 5
             msg += ". Incoming number is " + incomingNumber;
             
             // TODO This would be a good place to "Do something when the phone rings" ;-)
